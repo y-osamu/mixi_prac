a0cb6d19453c1878e52841336ff5abd6bf9750ca
osamu <osamu@samac.local>
2022-07-26 18:55:23 +0200

    status mangement

--- mixi/Views/RepoListView.swift
+++ mixi/Views/RepoListView.swift
@@ -9,19 +9,34 @@ import SwiftUI
 import MapKit
 
 struct RepoListView: View {
-    private let mockRepos: [Repo] = [
-        .mock1, .mock2, .mock3, .mock4, .mock5
-    ]
+    @State private var mockRepos: [Repo] = []
     
     var body : some View{
         NavigationView {
-            List(mockRepos) {repo in
-                NavigationLink(
-                    destination: RepoDetailView(repo: repo)) {
-                    RepoRow(repo: repo)
+            if mockRepos.isEmpty{
+                ProgressView("loading...")
+            }else{
+                List(mockRepos) {repo in
+                    NavigationLink(
+                        destination: RepoDetailView(repo: repo)) {
+                            RepoRow(repo: repo)
+                        }
                 }
+                .navigationTitle("Repogitoreis")
             }
-            .navigationTitle("Repogitoreis")
+        }
+        .onAppear {
+            loadRepos()
+        }
+        
+    }
+
+    private func loadRepos() {
+        // 1秒後にモックデータを読み込む
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            mockRepos = [
+                .mock1, .mock2, .mock3, .mock4, .mock5
+            ]
         }
     }
 }
